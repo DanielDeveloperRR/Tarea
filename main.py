cac14f57f18d5fe0247395f8f926bcc8caf50b22
Daniel <danielrodriguezrojas5@gmail.com>
2021-05-31 20:04:07 -0600

    Nuevos cambios

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 # Cajero automatico
-option = 0
-while option != -1:
+opcion = 0
+dinero = 100
+
+
+def retirar(plata=None):
+    global dinero
+    if plata == 0:
+        print("Agregue dinero primero")
+    else:
+        retiro = int(input("Digite el dinero que desea retirar:\n"))
+        if plata - retiro < 0:
+            print("Fondos insuficientes")
+        else:
+            dinero = dinero - retiro
+            print(dinero)
+
+
+while opcion != -1:
     option = int(input("Digite...\n(1) Para retirar dinero\n(2) Para consultar Cuenta\n(3) Para agregar Dinero\n(4) "
                        "Salir\n"))
 
     if option == 1:
-        print("1")
+        retirar(dinero)
     elif option == 2:
         print("2")
     elif option == 3:
@@ -14,10 +30,3 @@ while option != -1:
         option = -1
     else:
         print("Numero incorrecto!!")
-
-
-def control1():
-    print("pagina1")
-
-
-control1()
